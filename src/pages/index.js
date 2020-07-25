8332227f026ec6897a92d8a6f4959d55f63b2ef6
Matthew Ito <mito81@gmail.com>
2020-07-24 19:11:50 -0700

    Adding projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,33 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
+import "../scss/main.scss";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = () => {
+  return (
+    <div className="title-screen-page">
+      <div className="full-width-container">
+        <div class="title-name-holder">
+          <h1 className="title-header">Sophie</h1>
+          <h1 className="title-header">Studio</h1>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+    // <Layout>
+    //   <SEO title="Home" />
+    //   <h1>Hi people</h1>
+    //   <p>Welcome to your new Gatsby site.</p>
+    //   <p>Now go build something great.</p>
+    //   <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    //     <Image />
+    //   </div>
+    //   <Link to="/page-2/">Go to page 2</Link> <br />
+    //   <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    // </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
